0d2b942f60f7cd5d4f364dd35861f1385e645199
Koroliuk <koroliuk.yaroslav@gmail.com>
2021-06-25 10:39:14 +0300

    Update admin home page | Add users table

--- src/main/java/ua/training/model/repository/UserRepository.java
+++ src/main/java/ua/training/model/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package ua.training.model.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import ua.training.model.entity.User;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
     Optional<User> findByLogin(String login);
 }
