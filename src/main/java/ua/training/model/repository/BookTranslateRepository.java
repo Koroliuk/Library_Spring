0f1ccb4530639e890fc513dca2d5fd985fe08e0f
Koroliuk <koroliuk.yaroslav@gmail.com>
2021-07-05 11:42:54 +0300

    Refactor repositories

--- src/main/java/ua/training/model/repository/BookTranslateRepository.java
+++ src/main/java/ua/training/model/repository/BookTranslateRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ua.training.model.entity.Book;
 import ua.training.model.entity.BookTranslate;
@@ -21,20 +20,23 @@ public interface BookTranslateRepository extends CrudRepository<BookTranslate, L
 
     void deleteAllByBook(Book book);
 
-    List<BookTranslate> findAllByTitleContainsOrAuthorsStringContainsIgnoreCaseAndLanguage(
-            String title, String authorsString, Language language);
-
     Page<BookTranslate> findAllByLanguage(Language language, Pageable pageable);
 
-    @Query(value = "SELECT * from book_translate WHERE language_id = ?2 AND (title LIKE '%' || ?1 || '%' OR authors_string LIKE '%' || ?1 || '%')", nativeQuery = true)
+    @Query(value = "SELECT * from book_translate WHERE language_id = ?2 AND (title LIKE '%' || ?1 || '%' OR " +
+            "authors_string LIKE '%' || ?1 || '%')", nativeQuery = true)
     Page<BookTranslate> findAllByKeyWordAndLanguage(String keyWord, long languageId, Pageable pageable);
 
-    @Query(value = "SELECT * from book_translate WHERE language_id = ?2 AND (title LIKE '%' || ?1 || '%' OR authors_string LIKE '%' || ?1 || '%')", nativeQuery = true)
+    @Query(value = "SELECT * from book_translate WHERE language_id = ?2 AND (title LIKE '%' || ?1 || '%' OR " +
+            "authors_string LIKE '%' || ?1 || '%')", nativeQuery = true)
     List<BookTranslate> findAllByKeyWordAndLanguage(String keyWord, long languageId);
 
-    @Query(value = "SELECT * from book_translate WHERE language_id = ?2 AND (title LIKE '%' || ?1 || '%' OR authors_string LIKE '%' || ?1 || '%') ORDER BY (SELECT id FROM book WHERE book.id = book_translate.book_id)", nativeQuery = true)
+    @Query(value = "SELECT * from book_translate WHERE language_id = ?2 AND (title LIKE '%' || ?1 || '%' OR " +
+            "authors_string LIKE '%' || ?1 || '%') ORDER BY (SELECT id FROM book " +
+            "WHERE book.id = book_translate.book_id)", nativeQuery = true)
     Page<BookTranslate> findAllByKeyWordAndLanguageOrderByBookId(String keyWord, long languageId, Pageable pageable);
 
-    @Query(value = "SELECT * from book_translate WHERE language_id = ?2 AND (title LIKE '%' || ?1 || '%' OR authors_string LIKE '%' || ?1 || '%') ORDER BY (SELECT publication_date FROM book WHERE book.id = book_translate.book_id)", nativeQuery = true)
+    @Query(value = "SELECT * from book_translate WHERE language_id = ?2 AND (title LIKE '%' || ?1 || '%' OR " +
+            "authors_string LIKE '%' || ?1 || '%') ORDER BY (SELECT publication_date FROM book " +
+            "WHERE book.id = book_translate.book_id)", nativeQuery = true)
     Page<BookTranslate> findAllByKeyWordAndLanguageOrderByDate(String keyWord, long languageId, Pageable pageable);
 }
--- src/main/java/ua/training/model/repository/OrderRepository.java
+++ src/main/java/ua/training/model/repository/OrderRepository.java
@@ -12,11 +12,18 @@ import java.util.List;
 
 @Repository
 public interface OrderRepository extends PagingAndSortingRepository<Order, Long> {
-    Page<Order> findAllByUserAndOrderStatusOrOrderStatus(User user, OrderStatus orderStatus1, OrderStatus orderStatus2, Pageable paging);
+    Page<Order> findAllByUserAndOrderStatusOrOrderStatus(User user, OrderStatus orderStatus1, OrderStatus orderStatus2,
+                                                         Pageable paging);
+
     List<Order> findAllByUserAndOrderStatusOrOrderStatus(User user, OrderStatus orderStatus1, OrderStatus orderStatus2);
+
     Page<Order> findAllByUserAndOrderStatus(User user, OrderStatus orderStatus, Pageable paging);
+
     List<Order> findAllByUserAndOrderStatus(User user, OrderStatus orderStatus);
+
     List<Order> findAllByUser(User user);
+
     Page<Order> findAllByOrderStatus(OrderStatus orderStatus, Pageable paging);
+
     List<Order> findAllByOrderStatus(OrderStatus orderStatus);
 }
